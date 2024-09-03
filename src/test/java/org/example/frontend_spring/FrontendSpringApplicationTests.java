af3261e323f89334c8df722505028be4c37ca9d4
Alex <borovinskihalex986@gmail.com>
2024-09-03 08:53:02 +0500

    frontend ver0.1

---
+++ src/test/java/org/example/frontend_spring/FrontendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.frontend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
