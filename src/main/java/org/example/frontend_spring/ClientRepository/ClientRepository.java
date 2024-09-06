dfaa966a3b6da3f921f28b30967d22973c6275c9
AlexB986 <54481725+AlexB986@users.noreply.github.com>
2024-09-06 20:50:00 +0500

    Delete src/main/java/org/example/frontend_spring/ClientRepository directory

--- src/main/java/org/example/frontend_spring/ClientRepository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.frontend_spring.ClientRepository;
-import org.example.frontend_spring.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
